4044e300a73cd1f6e8174323aea463760517d403
Loi Pham <loitpham@gmail.com>
2021-06-06 23:17:22 -0700

    Built a list we can delete from.

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Loi Pham on 6/6/21.
+//
+
+import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text("\(item.name)")
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                                        expenses.items.append(expense)
+                                    }) {
+                                        Image(systemName: "plus")
+                                    }
+            )
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Loi Pham on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
